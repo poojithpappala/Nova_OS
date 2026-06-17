8843af58607987394ae97966c049a83d1b44b705
poojithpappala <poojithpappala@gmail.com>
2026-06-17 13:02:03 +0530

    added input & output redirection

--- test.c
+++ test.c
@@ -2,12 +2,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <signal.h>
+#include <fcntl.h>//for handling the file descriptors in redirections
 #include "token.h" 
 
 #define R_GREEN   "\033[32m"
 #define RESET     "\033[0m"
 #define R_YELLOW  "\033[33m"
+#define R_RED     "\033[31m"
 #define VERSION "v1.4"   
 
 //structure for handling stopped jobs
@@ -23,12 +26,12 @@ int main(){
     while(1){
 
         //checking background processes if any thing has completed 
-        pid_t completed_pid;
+        // pid_t completed_pid;
         int status;
 
-        while((completed_pid = waitpid(-1, &status, WNOHANG)) > 0){
-            printf("\nBackground job %d is finished!\n", completed_pid);
-        }
+        // while((completed_pid = waitpid(-1, &status, WNOHANG)) > 0){
+        //     printf("\nBackground job %d is finished!\n", completed_pid);
+        // }
         
         printf(R_GREEN"nova"RESET": ");
         
@@ -60,23 +63,23 @@ int main(){
             break;
         }
         //handling stopped fg jobs 
-        else if(strcmp(buffer, "fg") == 0){
-            if(stopped_jobs_count == 0){
-                printf("There are no foreground jobs running!\n");
-            }else{
-                stopped_jobs_count--;
-                pid_t pid = stopped_jobs[stopped_jobs_count].pid;
+        // else if(strcmp(buffer, "fg") == 0){
+        //     if(stopped_jobs_count == 0){
+        //         printf("There are no foreground jobs running!\n");
+        //     }else{
+        //         stopped_jobs_count--;
+        //         pid_t pid = stopped_jobs[stopped_jobs_count].pid;
 
-                printf("continuing %s", stopped_jobs[stopped_jobs_count].command);
+        //         printf("continuing %s", stopped_jobs[stopped_jobs_count].command);
 
-                kill(pid, SIGCONT);
-                waitpid(pid, &status, WUNTRACED);
-                if(WIFSTOPPED(status)){
-                    stopped_jobs_count++;
-                    printf("Process %d Stopped again!\n", pid);
-                }
-            }
-        }
+        //         kill(pid, SIGCONT);
+        //         waitpid(pid, &status, WUNTRACED);
+        //         if(WIFSTOPPED(status)){
+        //             stopped_jobs_count++;
+        //             printf("Process %d Stopped again!\n", pid);
+        //         }
+        //     }
+        // }
         else{
             //handle background process later
             int token_count = 0;
@@ -87,28 +90,100 @@ int main(){
                 continue;
             }
 
+            char *input_file = NULL;
+            char *output_file = NULL;
+            int append_mode = 0; // 1? '>>' : '>'(for append and output redirection mode)
+            int syntax_error = 0;
+
             char **args = malloc((token_count +1)* sizeof(char *));
             int arg_idx = 0;
 
             for(int t = 0; t<token_count; t++){
+
                 if(tokens[t].type == TOKEN_WORD){
                     args[arg_idx++] = tokens[t].value;
-                }else{
-                    //handle the special char later
-                    break;
+                }
+
+                else if(strcmp(tokens[t].value, "<") == 0){
+                    if(t+1 < token_count && tokens[t+1].type == TOKEN_WORD){
+                        input_file = tokens[++t].value;
+                    }else{
+                        printf(R_GREEN"Nova: "R_RED"syntax error near unexpected token '<'\n");
+                        syntax_error = 1;
+                        break;
+                    }
+                }
+
+                else if(strcmp(tokens[t].value, ">") == 0){
+                    if(t + 1 < token_count && tokens[t+1].type == TOKEN_WORD){
+                        output_file = tokens[++t].value;
+                    }else{
+                        printf(R_GREEN"Nova: "R_RED"syntax error near unexpected token '>'\n");
+                        syntax_error = 1;
+                        break;
+                    }
+                }
+                
+                else if(strcmp(tokens[t].value, ">>") == 0){
+                    if(t + 1 < token_count && tokens[t+1].type == TOKEN_WORD){
+                        output_file = tokens[++t].value;
+                        append_mode = 1;
+                    }else{
+                        printf(R_GREEN"Nova: "R_RED"syntax error near unexpected token '>>'\n");
+                        syntax_error = 1;
+                        break;
+                    }
+
                 }
 
             }
+
+            
             args[arg_idx] = NULL;
             fflush(stdout);
             
+            if(syntax_error){
+                for(int i =0; i< token_count; i++){
+                    free(tokens[i].value);
+                }
+                free(tokens);
+                free(args);
+                continue;
 
+            }
             pid_t pid = fork(); //childpid
 
             if(pid < 0 ){
                 perror("Fork Failed!!!");
             }
             else if(pid == 0){
+                if(input_file != NULL){
+                    int fd_in = open(input_file, O_RDONLY);
+                    if(fd_in < 0){
+                        perror(R_GREEN"Nova: "R_RED"input file error");
+                        _exit(1);
+                    }
+                    dup2(fd_in, STDIN_FILENO);
+                    close(fd_in);
+                }
+
+                if(output_file != NULL){
+                    int flags = O_WRONLY | O_CREAT;
+                    if(append_mode){
+                        flags |= O_APPEND;
+                    }else{
+                        flags |= O_TRUNC;
+                    }
+
+                    int fd_out = open(output_file, flags, 0644); //owner can create files but not the other users by using 0644
+                    if(fd_out < 0){
+                        perror(R_GREEN"Nova: "R_RED"output file error");
+                    }else{
+                        dup2(fd_out, STDOUT_FILENO);
+                        close(fd_out);
+                    }
+                }
+
                 if(arg_idx > 0 && execvp(args[0], args) < 0){
                     perror("command not found!");
                     _exit(1);
@@ -125,10 +200,11 @@ int main(){
                 _exit(0);
             }
             else{
-                    if(bg_process){
-                        printf("background process running....\n");
-                    }else{
-                        waitpid(pid, &status, 0);
+                    waitpid(pid, &status, 0);
+                    // if(bg_process){
+                    //     printf("background process running....\n");
+                    // }else{
+                    //     waitpid(pid, &status, 0);
                         // waitpid(pid, &status, WUNTRACED);
                         
                         // if(WIFSIGNALED(status)){
@@ -143,21 +219,19 @@ int main(){
                         //     }
                         // }
                     }
+                for (int t = 0; t < token_count; t++) {
+                    free(tokens[t].value);
+                }
+                free(tokens);
+                free(args);
             }
-            for (int t = 0; t < token_count; t++) {
-                free(tokens[t].value);
-            }
-            free(tokens);
-            free(args);
-
-
+            
         }
 
+        printf("\n\nThank you!\n\n");
+        fflush(stdout);
+        printf("system is shutting down...\n");
+        sleep(1);
+    
+        return 0;
     }
-    printf("\n\nThank you!\n\n");
-    fflush(stdout);
-    printf("system is shutting down...\n");
-    sleep(1);
-
-    return 0;
-}
